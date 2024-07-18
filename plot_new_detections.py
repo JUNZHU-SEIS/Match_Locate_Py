c9736860ff11ec4fe435d1ac2828cfab3af25ec2
JUNZHU-SEIS <Jun__Zhu@outlook.com>
2024-07-19 03:13:52 +0800

    initial

--- plot_new_detections.py
+++ plot_new_detections.py
@@ -17,21 +17,19 @@ else:
 	for x in os.listdir(savedir):os.unlink(os.path.join(savedir,x))
 vmodel = create_velocity_model(config['fvelo'],'eg.tvel')
 tplt = read_growclust(config['ctlg_path'])
-print(tplt)
 
-def plot_waveform():
-	df = pd.read_csv(os.path.join(root,'new.ctlg'),sep=' ')
-	for time,lat,lon,depth,i in zip(df['ot'],df['lat'],df['lon'],df['dep'],df['template_id']):
-		print('='*10+'\n',time,lat,lon,depth)
-		ot = UTCDateTime(time)
-		date = '%d%02d%02d'%(ot.year,ot.month,ot.day)
-		st = read(os.path.join(waveform_root,date,'*E'))
-		st = st.trim(ot,ot+100,pad=True,fill_value=0)
-		P,S,G = [],[],[]
-		for tr in st:
-			tr.stats.coordinates = {}
-			tr.stats.coordinates['latitude'] = tr.stats.sac['stla']
-			tr.stats.coordinates['longitude'] = tr.stats.sac['stlo']
+def plot_one_event(time,lat,lon,depth,offset_min=None,offset_max=None,start=None,duration=None):
+	print('='*10+'\n',time,lat,lon,depth)
+	ot = UTCDateTime(time)
+	date = '%d%02d%02d'%(ot.year,ot.month,ot.day)
+	st = read(os.path.join(waveform_root,date,'*E'))
+	st = st.trim(ot,ot+100,pad=True,fill_value=0)
+	if offset_min==None:P,S,G = [],[],[]
+	for tr in st:
+		tr.stats.coordinates = {}
+		tr.stats.coordinates['latitude'] = tr.stats.sac['stla']
+		tr.stats.coordinates['longitude'] = tr.stats.sac['stlo']
+		if offset_min==None:
 			gcarc = loc2deg(lon,lat,tr.stats.sac.stla,tr.stats.sac.stlo)
 			G.append(gcarc)
 			focal = depth + tr.stats.sac.stel/1e3
@@ -39,14 +37,28 @@ def plot_waveform():
 			ts = vmodel.get_travel_times(focal,distance_in_degree=gcarc,phase_list=['s','S'])
 			if len(tp):P.append(tp[0].time)
 			if len(ts):S.append(ts[0].time)
+	if offset_min==None:
 		P,S,G = np.array(P),np.array(S),np.array(G)
-		start = max(0,min(P)-3)
 		diff = np.median(np.diff(np.sort(G)))
-		offset_min,offset_max = np.sort(G)[1]-diff,np.sort(G)[-2]+diff
-		print(offset_max,offset_min,np.max(G),np.min(G),diff)
-		fig = st.plot(type='section',ev_coord=(lon,lat),dist_degree=True,handle=True,
-			orientation='horizontal',recordstart=start,recordlength=(min(100,max(S)+5)-start),
-			offset_min=offset_min,offset_max=offset_max)
-		plt.show()
+		start = max(0,min(P)-3)
+		duration = min(100,max(S)+5)-start
+		offset_min = np.sort(G)[1]-diff
+		offset_max = np.sort(G)[-2]+diff
+	fig = st.plot(type='section',ev_coord=(lon,lat),dist_degree=True,handle=True,
+		orientation='horizontal',recordstart=start,recordlength=duration,
+		offset_min=offset_min,offset_max=offset_max)
+	plt.tight_layout()
+	return fig,offset_min,offset_max,start,duration
+
+def plot_waveform():
+	df = pd.read_csv(os.path.join(root,'new.ctlg'),sep=' ')
+	for time,lat,lon,depth,i in zip(df['ot'],df['lat'],df['lon'],df['dep'],df['template_id']):
+		fig,offset_min,offset_max,start,duration = plot_one_event(time,lat,lon,depth) # need debug
+		fig.savefig(os.path.join(savedir,'%d_%s_candidate.pdf'%(i,time)))
+		plt.close()
+		evtp = tplt[tplt['evid']==i].iloc[0]
+		fig,_,_,_,_ = plot_one_event(evtp['time'],evtp['LON'],evtp['LAT'],evtp['DEPTH'],
+			offset_min=offset_min,offset_max=offset_max,start=start,duration=duration)
+		fig.savefig(os.path.join(savedir,'%d_%s_template.pdf'%(i,time)))
 		plt.close()
 plot_waveform()
